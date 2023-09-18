594fa56f42b3a70a2aa038040523dbfdd8619f21
Levente Major <leventemajor36@gmail.com>
2023-09-18 09:21:52 +0200

    Add project files.

---
+++ UltraBalaton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace UltraBalaton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var versenyzok = new List<Versenyzo>();
+            using var sr = new StreamReader(
+                path: "../../../src/ub2017egyeni.txt",
+                Encoding.UTF8);
+
+            string headerLine = sr.ReadLine();
+
+            while (!sr.EndOfStream) versenyzok.Add(new Versenyzo(sr.ReadLine()));
+
+            Console.WriteLine($"3.Feladat: Egyéni indulók: {versenyzok.Count} fő");
+        }
+    }
+}
---
+++ UltraBalaton/Versenyzo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraBalaton
+{
+    class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int VersenySzam { get; set; }
+        public bool Kategoria { get; set; }
+        public int[] Pontszamok { get; set; }
+        public int TeljesTav { get; set; }
+
+        public Versenyzo(string beolvasottSor)
+        {
+            string[] adat = beolvasottSor.Split(';');
+            this.Nev = adat[0];
+            this.VersenySzam = int.Parse(adat[1]);
+            this.Kategoria = adat[2] == "Ferfi";
+            this.Pontszamok = new int[3];
+            string[] pontok = adat[3].Split(':');
+            for (int i = 0; i < Pontszamok.Length; i++)
+            {
+                Pontszamok[i] = int.Parse(pontok[i]);
+            }
+            this.TeljesTav = int.Parse(adat[4]);
+        }
+    }
+}
